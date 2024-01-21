3dc993ebc4a43e5e334ccf132e39529845253f70
Jubo30 <julian.bornemann@gmx.net>
2024-01-21 16:43:55 +0100

    Initial commit

---
+++ src/client/java/com/mcmiddleearth/mcmeping/McMEPingClient.java
@@ -0,0 +1,10 @@
+package com.mcmiddleearth.mcmeping;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McMEPingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
